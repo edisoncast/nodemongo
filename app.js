bbf72e77fb46de096d8c2ba3f735459fb201fd70
juan55860 <juan55860@gmail.com>
2015-06-03 12:26:08 -0500

    insert locations devices to MONGODB

--- app.js
+++ app.js
@@ -1,41 +1,75 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+// requires mongodb
+var MongoClient = require('mongodb').MongoClient;
+var mongoDbObj;
+var assert      = require('assert');
+
+// requires server + socket
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var server      = require('http').createServer(app);
+var config      = require('./.env.json');
+var validator   = require('validator');
+var port        = 5003;
+
+server.listen(port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connection URL
-var url = 'mongodb://localhost:27017/ejemplo';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to Server");
- 
- insertDocuments(db, function() {
-   updateDocument(db, function() {
-   removeDocument(db, function() {
-         findDocuments(db, function() {
-           db.close();
-      });
-   });
-  });
- }); 
+var url = 'mongodb://'+config.host+':'+config.port+'/'+ config.scheme;
+
+function validateData(data)
+{
+    if(!validator.isNumeric(data.device_id))
+        return false;
+    if(!validator.isFloat(data.latitude))
+        return false;
+    if(!validator.isFloat(data.longitude))
+        return false;
+    return true;
+}
+
+app.post('/device/insert/location', function(req, res, next)
+{
+    res.setHeader('Content-Type', 'application/json');
+    data = req.body;
+    var validation = {};
+    validation.state = validateData(data);
+    if(validation.state) {
+       insert({ "device_id": data.device_id, "locs": [{"lng": parseFloat(data.longitude), "lat": parseFloat(data.latitude)}] });
+    }
+    res.send(validation);
+    res.end();
 });
 
-//Insertar un documento
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insert([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) 
+{
+    if(err){
+      console.log('error...', err);
+    }
+    else{
+      console.log("Connected correctly to Server");
+      mongoDbObj = {db: db, locations: db.collection('locations')};
+    }
+});
+
+
+function insert (device) 
+{
+    mongoDbObj.locations.insert(device, function(err, result) 
+    {
+      if (err) {
+        console.log("Inserting Error", err);
+      } else{
+        console.log('successfully inserted', device)
+      };    
+    });
 }
 
 
+/*
 //actualizar un documento
 
 var updateDocument = function(db, callback) {
@@ -78,4 +112,4 @@ var findDocuments = function(db, callback) {
     console.dir(docs);
     callback(docs);
   });
-}
+} */
